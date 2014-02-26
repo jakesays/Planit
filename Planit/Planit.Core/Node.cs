e2c398ea59dec9a195d10fc995506c22f2f77b4b
dglambert <dglambert@outlook.com>
2014-02-25 23:04:43 -0500

    added Planit.Core which contains the Node class. I am experimenting with this to be the collection for our project

---
+++ Planit/Planit.Core/Node.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planit.Core
+{
+    public class Node<T>
+    {
+        public T data;
+        LinkedList<Node<T>> children;
+
+        public Node(T data)
+        {
+            this.data = data;
+            children = new LinkedList<Node<T>>();
+        }
+
+        public void addChild(T data)
+        {
+            children.AddFirst(new Node<T>(data));
+        }
+
+        public bool hasChildren
+        { 
+            get {return children.Any();}
+        }
+
+        public Node<T> getChild(int i)
+        {
+            foreach (Node<T> n in children)
+                if (--i == 0) 
+                    return n;
+            return null;
+        }
+        
+    }
+}
